76e6557314eea1fc09a80b8eadd8742903754ade
Eran Hammer <eran@hammer.io>
2017-11-03 00:04:12 -0700

    Update deps. Closes #3

--- test/index.js
+++ test/index.js
@@ -20,47 +20,47 @@ const expect = Code.expect;
 
 describe('type()', () => {
 
-    it('parses header', async () => {
+    it('parses header', () => {
 
         const type = Content.type('application/json; some=property; and="another"');
         expect(type.mime).to.equal('application/json');
         expect(type.boundary).to.not.exist();
     });
 
-    it('parses header (only type)', async () => {
+    it('parses header (only type)', () => {
 
         const type = Content.type('application/json');
         expect(type.mime).to.equal('application/json');
         expect(type.boundary).to.not.exist();
     });
 
-    it('parses header (boundary)', async () => {
+    it('parses header (boundary)', () => {
 
         const type = Content.type('application/json; boundary=abcdefghijklm');
         expect(type.mime).to.equal('application/json');
         expect(type.boundary).to.equal('abcdefghijklm');
     });
 
-    it('parses header (quoted boundary)', async () => {
+    it('parses header (quoted boundary)', () => {
 
         const type = Content.type('application/json; boundary="abcdefghijklm"');
         expect(type.mime).to.equal('application/json');
         expect(type.boundary).to.equal('abcdefghijklm');
     });
 
-    it('handles large number of OWS', async () => {
+    it('handles large number of OWS', () => {
 
         const now = Date.now();
         expect(() => Content.type(`l/h ; ${new Array(80000).join(' ')}"`)).to.throw();
         expect(Date.now() - now).to.be.below(100);
     });
 
-    it('errors on invalid header', async () => {
+    it('errors on invalid header', () => {
 
         expect(() => Content.type('application/json; some')).to.throw();
     });
 
-    it('errors on multipart missing boundary', async () => {
+    it('errors on multipart missing boundary', () => {
 
         expect(() => Content.type('multipart/form-data')).to.throw();
     });
@@ -68,35 +68,35 @@ describe('type()', () => {
 
 describe('disposition()', () => {
 
-    it('parses header', async () => {
+    it('parses header', () => {
 
         const header = 'form-data; name="file"; filename=file.jpg';
 
         expect(Content.disposition(header)).to.equal({ name: 'file', filename: 'file.jpg' });
     });
 
-    it('parses header (empty filename)', async () => {
+    it('parses header (empty filename)', () => {
 
         const header = 'form-data; name="file"; filename=""';
 
         expect(Content.disposition(header)).to.equal({ name: 'file', filename: '' });
     });
 
-    it('parses header (filename with quotes)', async () => {
+    it('parses header (filename with quotes)', () => {
 
         const header = 'form-data; name="file"; filename="fi\'l\'e.jpg"';
 
         expect(Content.disposition(header)).to.equal({ name: 'file', filename: 'fi\'l\'e.jpg' });
     });
 
-    it('handles language filename', async () => {
+    it('handles language filename', () => {
 
         const header = 'form-data; name="file"; filename*=utf-8\'en\'with%20space';
 
         expect(Content.disposition(header)).to.equal({ name: 'file', filename: 'with space' });
     });
 
-    it('handles large number of OWS', async () => {
+    it('handles large number of OWS', () => {
 
         const now = Date.now();
         const header = `form-data; x; ${new Array(5000).join(' ')};`;
@@ -104,42 +104,42 @@ describe('disposition()', () => {
         expect(Date.now() - now).to.be.below(100);
     });
 
-    it('errors on invalid language filename', async () => {
+    it('errors on invalid language filename', () => {
 
         const header = 'form-data; name="file"; filename*=steve';
         expect(() => Content.disposition(header)).to.throw('Invalid content-disposition header format includes invalid parameters');
     });
 
-    it('errors on invalid format', async () => {
+    it('errors on invalid format', () => {
 
         const header = 'steve';
         expect(() => Content.disposition(header)).to.throw('Invalid content-disposition header format');
     });
 
-    it('errors on missing header', async () => {
+    it('errors on missing header', () => {
 
         expect(() => Content.disposition('')).to.throw('Missing content-disposition header');
     });
 
-    it('errors on missing parameters', async () => {
+    it('errors on missing parameters', () => {
 
         const header = 'form-data';
         expect(() => Content.disposition(header)).to.throw('Invalid content-disposition header missing parameters');
     });
 
-    it('errors on missing language value', async () => {
+    it('errors on missing language value', () => {
 
         const header = 'form-data; name="file"; filename*=';
         expect(() => Content.disposition(header)).to.throw('Invalid content-disposition header format includes invalid parameters');
     });
 
-    it('errors on invalid percent encoded language value', async () => {
+    it('errors on invalid percent encoded language value', () => {
 
         const header = 'form-data; name="file"; filename*=utf-8\'en\'with%vxspace';
         expect(() => Content.disposition(header)).to.throw('Invalid content-disposition header format includes invalid parameters');
     });
 
-    it('errors on missing name', async () => {
+    it('errors on missing name', () => {
 
         const header = 'form-data; filename=x';
         expect(() => Content.disposition(header)).to.throw('Invalid content-disposition header missing name parameter');
